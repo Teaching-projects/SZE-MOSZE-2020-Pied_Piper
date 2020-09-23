75afa90021012a6ed9a61b017a47f03b42748a8a
Kocsis David <david007214@gmail.com>
2020-09-23 20:21:17 +0200

    Unit teszt beta cuccok

---
+++ proba.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a, b;
+    cout << "Adj meg ket szamot."<<endl;
+    cin >>a>>b;
+    cout<<endl;
+    cout << 2*a << ' '<< 2*b<<endl<<endl;
+    return 0;
+}
